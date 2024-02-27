43ce8a6dd1e0685a645db8ee06905ab5b5c492f2
kiselev <georgiy.kiselev@loginom.ru>
2024-02-27 10:08:43 +0300

    Docs and refactoring

--- index.js
+++ index.js
@@ -38,22 +38,27 @@ const fs = require("fs"),
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
 
+/**
+ * The `Cache` class is a cache implementation that allows storing and retrieving data with an optional time-to-live (TTL) value.
+ * It uses the `flat-cache` library to persist the cache data to disk.
+ */
 class Cache {
+  /**
+   * Initializes a new cache instance with the provided options.
+   * @param {Object} options - The cache options.
+   * @param {string} options.ns - The namespace for the cache.
+   * @param {string} options.dir - The directory path to store the cache data.
+   * @param {number} options.ttl - The default time-to-live (TTL) value for cached data.
+   */
   constructor(options) {
-
     options = Object.assign({ ns: "default", ttl: null }, options);
-
     let { ns, dir, ttl } = options;
 
     ns = ns || "default";
-
     dir = dir && fs.existsSync(path.dirname(dir)) ? dir : null;
-
     dir = path.join(os.tmpdir(), "cache", ns);
 
-    // loads the cache, if one does not exists for the given
     this.defaultTTL = isNaN(Number(ttl)) ? null : Number(ttl);
-
     this.cache = flatCache.load(ns, dir);
 
     this.getKey = this.get;
@@ -63,12 +68,17 @@ class Cache {
     this.clearCacheById = this.cache.clearCacheById;
   }
 
-  __is_expired(expires, returnTTL = false) {
-    let exp, now;
-
+  /**
+   * Checks if a key has expired.
+   * @private
+   * @param {Date} expires - The expiration date of the key.
+   * @param {boolean} returnTTL - Whether to return the remaining time-to-live (TTL) as a human-readable string.
+   * @returns {boolean|string} - True if the key has expired, or the remaining time-to-live (TTL) as a string if returnTTL is true.
+   */
+  __isExpired(expires, returnTTL = false) {
     if (expires) {
-      exp = dayjs(expires);
-      now = dayjs();
+      const exp = dayjs(expires);
+      const now = dayjs();
 
       if (returnTTL) {
         return dayjs.duration(exp.diff(now)).humanize(true);
@@ -80,29 +90,35 @@ class Cache {
     return false;
   }
 
-  __invalidate_key(key) {
-
-    //
-    let data = this.cache.getKey(key);
+  /**
+   * Invalidates a key if it has expired.
+   * @private
+   * @param {string} key - The key to invalidate.
+   * @returns {string|null} - The remaining time-to-live (TTL) as a string if the key has not expired, or null if the key has expired.
+   */
+  __invalidateKey(key) {
+    const data = this.cache.getKey(key);
 
     if (data && data.expires) {
-      // check if time has expired
-      if (this.__is_expired(data.expires)) {
-        // remove key
+      if (this.__isExpired(data.expires)) {
         this.cache.removeKey(key);
         return null;
       } else {
-        let resp = this.__is_expired(data.expires, true);
-        return resp ? resp : null;
+        const remainingTTL = this.__isExpired(data.expires, true);
+        return remainingTTL ? remainingTTL : null;
       }
     }
   }
 
+  /**
+   * Returns all the data stored in the cache.
+   * @returns {Object} - An object with all the cached data.
+   */
   all() {
-    let data = this.cache.all();
+    const data = this.cache.all();
 
-    for (let key in data) {
-      if (this.__is_expired(data.expires)) {
+    for (const key in data) {
+      if (this.__isExpired(data[key].expires)) {
         data[key] = null;
         this.cache.removeKey(key);
       } else {
@@ -113,43 +129,52 @@ class Cache {
     return data;
   }
 
+  /**
+   * Retrieves the value associated with the specified key from the cache.
+   * @param {string} key - The key to retrieve the value for.
+   * @returns {*} - The value associated with the key, or null if the key is expired or does not exist.
+   */
   get(key) {
-
-    this.__invalidate_key(key);
-
-    let data = this.cache.getKey(key);
-
-    if (data) {
-      return data.value;
-    }
-
-    return null;
+    this.__invalidateKey(key);
+    const data = this.cache.getKey(key);
+    return data ? data.value : null;
   }
 
+  /**
+   * Deletes the value associated with the specified key from the cache.
+   * @param {string} key - The key to delete.
+   * @returns {boolean} - True if the key was deleted, false otherwise.
+   */
   del(key) {
-
     return this.cache.removeKey(key);
   }
 
+  /**
+   * Returns the remaining time-to-live (TTL) of the specified key.
+   * @param {string} key - The key to get the remaining TTL for.
+   * @returns {Object} - An object containing the key and the remaining time-to-live (TTL) as a string, or null if the key is expired or does not exist.
+   */
   ttl(key) {
-    return { key, expires: this.__invalidate_key(key) };
+    return { key, expires: this.__invalidateKey(key) };
   }
 
+  /**
+   * Sets the value associated with the specified key in the cache.
+   * @param {string} key - The key to set the value for.
+   * @param {*} value - The value to set.
+   * @param {number} [ttl] - The time-to-live (TTL) value for the key. If not provided, the default TTL will be used.
+   * @returns {boolean} - True if the value was set successfully, false otherwise.
+   */
   set(key, value, ttl) {
+    ttl = to_num(ttl) || to_num(this.defaultTTL);
 
-    let self = this;
-
-    ttl = to_num(ttl);
-    ttl =  ttl || to_num(this.defaultTTL);
-
-    let data = {
+    const data = {
       expires: ttl ? dayjs().add(ttl, "seconds").toDate() : null,
       value,
     };
 
-    self.cache.setKey(key, data);
-
-    return self.cache.save();
+    this.cache.setKey(key, data);
+    return this.cache.save();
   }
 }
 
